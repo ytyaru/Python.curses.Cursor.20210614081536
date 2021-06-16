0c23311cb6bf3996d59464547624515cbf79d71b
ytyaru <ytyaru@outlook.jp>
2021-06-16 16:26:26 +0900

    add memo

---
+++ src/layout/1/Layout.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding: utf8
+import curses, Curses
+# 複数のwindow/padをつくるときの配置を簡略化する。
+#   始点座標、サイズを画面比率で調整する。
+#   pad, win = LeftRightLayout(Pad, Window, 50%, 50%)
+#   レイアウトタイプは以下のとおり。
+#   * 横方向（左から右へ）
+#   * 縦方向（上から下へ）
+class 
