d75ebe29d8cd7a3d5263f45ca4a0e79c83f0f86e
ytyaru <ytyaru@outlook.jp>
2021-06-17 14:04:12 +0900

    mod

---
+++ src/layout/reserch/winpad/4.py
@@ -0,0 +1,527 @@
+#!/usr/bin/env python3
+# coding: utf8
+import os, curses, curses.panel
+# cursesライブラリを使いやすくラップする。newpad()で得たPadはmvwin()できず(0,0)固定でしか使えないことが発覚した。Windowとおなじように使えないことから設計しなおす必要が出てきた。
+class Curses:
+    screen = None
+    init = None
+    wait_time = 5
+    @classmethod
+    def run(cls, init=None, wait_time=5):
+        cls.init = init
+        cls.wait_time = wait_time
+        curses.wrapper(Curses.__main)
+    @classmethod
+    def __main(cls, screen, *args, **kwargs):
+        cls.screen = screen
+        Cursor.hide()
+        Curses.__init_color_pair()
+        if cls.init is not None: cls.init()
+        Window.inits()
+        Pad.inits()
+        cls.__draw()
+        cls.__loop()
+    @classmethod
+    def __init_color_pair(cls):
+        curses.setupterm('xterm-256color')
+        if not curses.has_colors(): raise Exception('このターミナルは色を表示できません。')
+        if not curses.can_change_color(): raise Exception('このターミナルは色を変更できません。')
+        curses.use_default_colors()
+        for i in range(1, curses.COLORS):
+            curses.init_pair(i, i, curses.COLOR_BLACK)
+    @classmethod
+    def __draw(cls):
+        if 0 == len(Pad.Pads): cls.screen.clear()
+        Window.draws()
+        Pad.draws()
+        curses.panel.update_panels()
+        curses.doupdate()
+    @classmethod
+    def __loop(cls):
+        cls.__draw()
+        if 0 < len(Pad.Pads): Pad.Pads[0].Pointer.keypad(True)
+        is_loop = True
+        while is_loop:
+            if 0 < len(Pad.Pads): key = Pad.Pads[0].Pointer.getch()
+            else: key = cls.screen.getch()
+            is_loop = Input.inputs(key)
+            cls.__draw()
+            curses.napms(cls.wait_time)
+
+class Terminal:
+    @property
+    def Name(self): return curses.termname()
+    @Name.setter
+    def Name(self, v): curses.setupterm(term=v)
+    @property
+    def Attrs(self): return curses.termattrs()
+    def get_capability(self, capname):
+        flag = curses.tigetflag(capname)
+        if -1 != flag: return flag
+        num = curses.tigetnum(capname)
+        if -2 != num : return num
+        s = curses.tigetstr(capname)
+        return s
+    def get_parameter(self, s, *args): # tparm(tigetstr("cup"), 5, 3) -> b'\033[6;4H'
+        return curses.tparm(s, *args)
+Terminal = Terminal()
+
+class Input:
+    Inputs = []
+    @classmethod
+    def inputs(cls, key):
+        if 0 == len(Input.Inputs): return False
+        for i in Input.Inputs:
+            is_loop = i.input(key)
+            if not is_loop: return False
+        return True
+    def __init__(self):
+        Input.Inputs.append(self)
+    def input(self, key): return False
+
+class Window:
+    Windows = []
+    @classmethod
+    def inits(cls):
+        for w in Window.Windows:
+            w.init()
+            for s in w.Subs:
+                s.init()
+    @classmethod
+    def draws(cls):
+        for w in Window.Windows:
+            w.Pointer.noutrefresh()
+            w.draw();
+            for s in w.Subs:
+                s.Pointer.noutrefresh()
+                s.draw()
+
+    def __init__(self, x=0, y=0, w=-1, h=-1):
+        self.__make_win(x, y, w, h)
+        self.__subs = []
+        self.__cursor = Cursor(self.__window)
+        Window.Windows.append(self)
+    @property
+    def Panel(self): return self.__panel
+    @property
+    def Pointer(self): return self.__window
+    @property
+    def Subs(self): return self.__subs
+    @property
+    def Cursor(self): return self.__cursor
+
+    @property
+    def X(self): return self.__window.getbegyx()[1]
+    @property
+    def Y(self): return self.__window.getbegyx()[0]
+    @X.setter
+    def X(self, v): self.__panel.move(self.Y, v); curses.panel.update_panels();
+    @Y.setter
+    def Y(self, v): self.__panel.move(v, self.X); curses.panel.update_panels();
+    @property
+    def W(self): return self.__window.getmaxyx()[1]
+    @property
+    def H(self): return self.__window.getmaxyx()[0]
+    @W.setter
+    def W(self, v): self.__window.resize(self.H, v); curses.panel.update_panels();
+    @H.setter
+    def H(self, v): self.__window.resize(v, self.W); curses.panel.update_panels();
+    def __make_win(self, x=0, y=0, w=-1, h=-1):
+        h = h if 0 < h and h <= curses.LINES else curses.LINES
+        w = w if 0 < w and w <= curses.COLS else curses.COLS
+        y = y if 0 <= y else 0
+        y = y if y <= curses.LINES - h else curses.LINES - h
+        x = x if 0 <= x else 0
+        x = x if x <= curses.COLS - w else curses.COLS - w
+        self.__window = curses.newwin(h, w, y, x)
+        self.__panel = curses.panel.new_panel(self.__window)
+    def make_sub(self, x=0, y=0, w=-1, h=-1, is_derwin=True):
+        self.__subs.append(SubWindow(self.Pointer,x=x,y=y,w=w,h=h,is_derwin=is_derwin))
+        return self.__subs[-1]
+    def show(self): self.__panel.show(); curses.panel.update_panels();
+    def hide(self): self.__panel.hide(); curses.panel.update_panels();
+    def switch(self):
+        if self.__panel.hidden(): self.__panel.show()
+        else: self.__panel.hide()
+        curses.panel.update_panels()
+    def init(self): pass
+    def draw(self): pass
+
+class SubWindow:
+    def __init__(self, parent, x=0, y=0, w=-1, h=-1, is_derwin=True):
+        self.__parent = parent.Pointer
+        self.__window = self.__make(x=x, y=y, w=w, h=h, is_derwin=is_derwin)
+        self.__cursor = Cursor(self.__window)
+        parent.Subs.append(self)
+    def __make(self, x=0, y=0, w=-1, h=-1, is_derwin=True):
+        ph, pw = self.__parent.getmaxyx()
+        h = h if 0 < h and h <= ph else ph
+        w = w if 0 < w and w <= pw else pw
+        y = y if 0 <= y else 0
+        y = y if 0 <= y and y < ph - h else ph - h
+        x = x if 0 <= x else 0
+        x = x if 0 <= x and x < pw - w else pw - w
+        return self.__parent.derwin(h, w, y, x) if is_derwin else self.__parent.subwin(h, w, y, x)
+    @property
+    def Pointer(self): return self.__window
+    @property
+    def Cursor(self): return self.__cursor
+
+    @property
+    def X(self): return self.__window.getbegyx()[1]
+    @property
+    def Y(self): return self.__window.getbegyx()[0]
+    @X.setter
+    def X(self, v): self.__window.mvwin(self.Y, v)
+    @Y.setter
+    def Y(self, v): self.__window.mvwin(v, self.X)
+
+    @property
+    def FromParentX(self): return self.__window.getparyx()[1]
+    @property
+    def FromParentY(self): return self.__window.getparyx()[0]
+    @FromParentX.setter
+    def FromParentX(self, v): self.__window.mvderwin(self.Y, v)
+    @FromParentY.setter
+    def FromParentY(self, v): self.__window.mvderwin(v, self.X)
+
+    @property
+    def W(self): return self.__window.getmaxyx()[1]
+    @property
+    def H(self): return self.__window.getmaxyx()[0]
+    @W.setter
+    def W(self, v): self.__window.resize(self.H, v)
+    @H.setter
+    def H(self, v): self.__window.resize(v, self.W)
+
+    def init(self): pass
+    def draw(self): pass
+
+# newpad()はいくつも問題がある。座標指定できない。Panelが使えない。よってWindowとおなじようには使えない。
+class SubPad:
+    def __init__(self, parent, x=0, y=0, w=-1, h=-1):
+        self.__parent = parent
+        self.__make(x, y, w, h)
+        self.__cursor = Cursor(self.__window)
+        self.__showX = 0
+        self.__showY = 0
+        parent.Subs.append(self)
+    @property
+    def Pointer(self): return self.__window
+    @property
+    def Parent(self): return self.__parent
+    @property
+    def Cursor(self): return self.__cursor
+
+    @property
+    def X(self): return self.__window.getbegyx()[1]
+    @property
+    def Y(self): return self.__window.getbegyx()[0]
+    @X.setter
+    def X(self, v): self.__window.derwin(self.Y, v)
+    @Y.setter
+    def Y(self, v): self.__window.derwin(v, self.X)
+#    @X.setter
+#    def X(self, v): self.__window.mvwin(self.Y, v)
+#    @Y.setter
+#    def Y(self, v): self.__window.mvwin(v, self.X)
+
+    @property
+    def ShowX(self): return self.__showX
+    @property
+    def ShowY(self): return self.__showY
+    @ShowX.setter
+    def ShowX(self, v): self.__showX = v
+    @ShowY.setter
+    def ShowY(self, v): self.__showY = v
+
+    @property
+    def W(self): return self.__window.getmaxyx()[1]
+    @property
+    def H(self): return self.__window.getmaxyx()[0]
+    @W.setter
+    def W(self, v): self.__window.resize(self.H, v)
+    @H.setter
+    def H(self, v): self.__window.resize(v, self.W)
+    def __make(self, x=0, y=0, w=-1, h=-1):
+        ph, pw = self.__parent.Pointer.getmaxyx()
+        h = h if 0 < h and h <= ph else ph
+        w = w if 0 < w and w <= pw else pw
+        y = y if 0 <= y else 0
+        y = y if 0 <= y and y < ph - h else ph - h
+        x = x if 0 <= x else 0
+        x = x if 0 <= x and x < pw - w else pw - w
+        self.__window = self.__parent.Pointer.subpad(h, w, y, x)
+        return self.__window
+    def noutrefresh(self):
+        self.__window.noutrefresh(self.__showY, self.__showX, self.Y, self.X, curses.LINES-1, curses.COLS-1)
+    def refresh(self):
+        self.__window.refresh(self.__showY, self.__showX, self.Y, self.X, curses.LINES-1, curses.COLS-1)
+    def init(self): pass
+    def draw(self): pass
+
+# newpad()はいくつも問題がある。座標指定できない。Panelが使えない。よってWindowとおなじようには使えない。
+class Pad:
+    Pads = []
+    @classmethod
+    def inits(cls):
+        for p in Pad.Pads:
+            p.init()
+            for s in p.Subs:
+                s.init()
+    @classmethod
+    def draws(cls):
+        for p in Pad.Pads:
+            p.noutrefresh()
+            p.draw();
+#            for s in p.Subs:
+#                s.noutrefresh()
+#                s.draw()
+    def __init__(self, w=-1, h=-1):
+        self.__make_win(w, h)
+        self.__subs = []
+        self.__cursor = Cursor(self.__window)
+        self.__showX = 0
+        self.__showY = 0
+        Pad.Pads.append(self)
+    @property
+    def Pointer(self): return self.__window
+    @property
+    def Subs(self): return self.__subs
+    @property
+    def Cursor(self): return self.__cursor
+
+    @property
+    def X(self): return self.__window.getbegyx()[1]
+    @property
+    def Y(self): return self.__window.getbegyx()[0]
+    @X.setter
+    def X(self, v): self.__window.mvwin(self.Y, v)
+    @Y.setter
+    def Y(self, v): self.__window.mvwin(v, self.X)
+
+    @property
+    def ShowX(self): return self.__showX
+    @property
+    def ShowY(self): return self.__showY
+    @ShowX.setter
+    def ShowX(self, v): self.__showX = v
+    @ShowY.setter
+    def ShowY(self, v): self.__showY = v
+
+    @property
+    def W(self): return self.__window.getmaxyx()[1]
+    @property
+    def H(self): return self.__window.getmaxyx()[0]
+    @W.setter
+    def W(self, v): self.__window.resize(self.H, v); curses.panel.update_panels();
+    @H.setter
+    def H(self, v): self.__window.resize(v, self.W); curses.panel.update_panels();
+    def __make_win(self, w=-1, h=-1):
+        h = h if 0 < h else curses.LINES
+        w = w if 0 < w else curses.COLS
+        self.__window = curses.newpad(h, w)
+    def noutrefresh(self):
+        self.__window.noutrefresh(self.__showY, self.__showX, self.Y, self.X, curses.LINES-1, curses.COLS-1)
+    def refresh(self):
+        self.__window.refresh(self.__showY, self.__showX, self.Y, self.X, curses.LINES-1, curses.COLS-1)
+    def init(self): pass
+    def draw(self): pass
+
+class ListWindow:
+    def __init__(self, items, x=0, y=0, w=-1, h=-1, attr=0):
+        self.__attr = attr
+        self.__make_win(items, x=x, y=y, w=w, h=h)
+        self.__subs = []
+        self.__cursor = Cursor(self.__window)
+        Window.Windows.append(self)
+        self.addstrs()
+    @property
+    def Pointer(self): return self.__window
+    @property
+    def Panel(self): return self.__panel
+    @property
+    def Subs(self): return self.__subs
+    @property
+    def Cursor(self): return self.__cursor
+    @property
+    def Items(self): return self.__items
+    @property
+    def Attr(self): return self.__attr
+    def addstrs(self):
+        self.__window.erase()
+        self.__window.bkgd(' ', curses.A_REVERSE | curses.color_pair(1))
+        for i, item in enumerate(self.Items):
+            s = f'{self.Items[i + self.ShowY][self.ShowX:]}'
+#            s = f'{self.Items[i + self.ShowY][self.ShowX:]} {self.ShowY},{self.ShowX} {self.Y},{self.X}'
+#            if self.W < len(s): s = s[:self.W-1]
+            s = s[:self.W-1]
+            self.__window.addstr(i, 0, s, self.Attr)
+            if self.H -1 <= i : break
+    def noutrefresh(self): self.__window.noutrefresh()
+    def refresh(self): self.__window.refresh()
+    def clear(self): self.__window.clear()
+    def erase(self): self.__window.erase()
+
+    @property
+    def W(self): return self.__w
+    @property
+    def H(self): return self.__h
+    @W.setter
+    def W(self, w): self.__w = w if 0 < w and w <= curses.COLS else curses.COLS
+    @H.setter
+    def H(self, h): self.__h = h if 0 < h and h <= curses.LINES else curses.LINES
+
+    @property
+    def X(self): return self.__window.getbegyx()[1]
+    @property
+    def Y(self): return self.__window.getbegyx()[0]
+    @X.setter
+    def X(self, x):
+        if   x < 0: self.__x = 0
+        elif curses.COLS < x + self.W: self.__x = curses.COLS - self.W
+        else: self.__x = x
+        self.__panel.move(self.Y, self.__x)
+        curses.panel.update_panels();
+    @Y.setter
+    def Y(self, y):
+        if   y < 0: self.__y = 0
+        elif curses.LINES < y + self.H: self.__y = curses.LINES - self.H
+        else: self.__y = y
+        self.__panel.move(self.__y, self.X)
+        curses.panel.update_panels()
+    @property
+    def ShowX(self): return self.__showX
+    @property
+    def ShowY(self): return self.__showY
+    @ShowX.setter
+    def ShowX(self, x):
+        l = max(len(i) for i in self.Items)
+        if   x < 0: self.__showX = 0
+#        elif l < x: self.__showX = l
+        elif l < x: self.__showX = l - self.W + 1
+        elif l < x + self.W: self.__showX = l - self.W + 1
+        else: self.__showX = x
+    @ShowY.setter
+    def ShowY(self, y):
+        if   y < 0: self.__showY = 0
+        elif len(self.Items) -self.H - 1 < y: self.__showY = len(self.Items) - self.H
+        else: self.__showY = y
+    def __make_win(self, items, x=0, y=0, w=-1, h=-1):
+        self.__items = items
+        self.W = w
+        self.H = h
+        if   x < 0: self.__x = 0
+        elif curses.COLS < x + self.W: self.__x = curses.COLS - self.W
+        else: self.__x = x
+        if   y < 0: self.__y = 0
+        elif curses.LINES < y + self.H: self.__y = curses.LINES - self.H
+        else: self.__y = y
+        self.ShowX = 0
+        self.ShowY = 0
+        self.__window = curses.newwin(self.H, self.W, self.__y, self.__x)
+        self.__panel = curses.panel.new_panel(self.__window)
+        self.__panel.move(self.__y, self.__x)
+        curses.panel.update_panels()
+    def show(self): self.__panel.show(); curses.panel.update_panels();
+    def hide(self): self.__panel.hide(); curses.panel.update_panels();
+    def switch(self):
+        if self.__panel.hidden(): self.__panel.show()
+        else: self.__panel.hide()
+        curses.panel.update_panels()
+    def init(self): pass
+    def draw(self): self.addstrs()
+
+class Cursor:
+    @classmethod
+    def hide(cls): curses.curs_set(0)
+    @classmethod
+    def show(cls, is_underline=False):
+        if is_underline: curses.curs_set(1)
+        else: curses.curs_set(2)
+    def __init__(self, window):
+        self.__window = window
+    @property
+    def X(self): return self.__window.getsyx()[1]
+    @property
+    def Y(self): return self.__window.getsyx()[0]
+    @X.setter
+    def X(self, v): self.__window.move(self.Y, v)
+    @Y.setter
+    def Y(self, v): self.__window.move(v, self.X)
+    def synchronize(self): self.__window.cursyncup()
+
+if __name__ == "__main__":
+    class Pad1(Pad):
+        def init(self):
+            self.Pointer.clear()
+            self.Pointer.bkgd(' ', curses.A_REVERSE | curses.color_pair(4))
+            for i in range(0, self.H):
+                self.Pointer.addstr(i, 0, f'Pad-1 {i} {self.ShowX},{self.ShowY} ({self.X},{self.Y}) {self.W},{self.H}', curses.A_REVERSE | curses.color_pair(4))
+    class SubPad1(SubPad):
+        def init(self):
+            self.Pointer.clear()
+            self.Pointer.bkgd(' ', curses.A_REVERSE | curses.color_pair(7))
+            self.Pointer.addstr(
+                0,0,
+                f'SubPad-1', 
+                curses.A_REVERSE | curses.color_pair(7))
+    class Window1(Window):
+        def draw(self):
+            self.Pointer.clear()
+            self.Pointer.bkgd(' ', curses.A_REVERSE | curses.color_pair(5))
+            self.Pointer.addstr(
+                0,0,
+                f'Window-1', 
+                curses.A_REVERSE | curses.color_pair(5))
+            self.Pointer.addstr(
+                1,0,
+                f'{Pad.Pads[0].ShowX},{Pad.Pads[0].ShowY} ({self.X},{self.Y}) {self.W},{self.H}', 
+                curses.A_REVERSE | curses.color_pair(5))
+    class SubWindow1(SubWindow):
+        def draw(self):
+            self.Pointer.clear()
+            self.Pointer.bkgd(' ', curses.A_REVERSE | curses.color_pair(6))
+            self.Pointer.addstr(
+                0,0,
+                f'SubWindow-1', 
+                curses.A_REVERSE | curses.color_pair(6))
+    class KeyInput(Input):
+        def input(self, key):
+            if curses.KEY_UP == key: Pad.Pads[0].ShowY -= 1 if 0 < Pad.Pads[0].ShowY else 0
+            elif curses.KEY_DOWN == key: Pad.Pads[0].ShowY += 1 if Pad.Pads[0].ShowY < Pad.Pads[0].H-curses.LINES else 0
+            elif curses.KEY_LEFT == key: Pad.Pads[0].ShowX -= 1 if 0 < Pad.Pads[0].ShowX else 0
+            elif curses.KEY_RIGHT == key: Pad.Pads[0].ShowX += 1 if Pad.Pads[0].ShowX < Pad.Pads[0].W-curses.COLS else 0
+            else: return False
+            return True
+
+    class ListKeyInput(Input):
+        def input(self, key):
+            if curses.KEY_UP == key: Window.Windows[0].ShowY -= 1
+            elif curses.KEY_DOWN == key: Window.Windows[0].ShowY += 1
+            elif curses.KEY_LEFT == key: Window.Windows[0].ShowX -= 1
+            elif curses.KEY_RIGHT == key: Window.Windows[0].ShowX += 1
+            else: return False
+            return True
+
+    def init():
+#        pad_w = int(curses.COLS/3)
+#        pad = Pad1(w=pad_w, h=curses.LINES*3)
+#        subpad = SubPad1(pad, x=2, y=2, w=int(pad_w/2), h=int(curses.LINES*3/3))
+#        win = Window1(x=pad_w+1, y=0, w=curses.COLS-(pad_w+1), h=curses.LINES)
+#        subwin = SubWindow1(win, x=2, y=2, w=20, h=5)
+#        KeyInput()
+
+#        lw = ListWindow([str(i) for i in range(curses.LINES*3)], x=2, y=4)
+#        lw = ListWindow([f'item-item-item-{i}' for i in range(curses.LINES*3)], x=2, y=4, w=20, h=10)
+        l = [f'item-item-item-{i}' for i in range(curses.LINES*3)]
+        l.insert(0, '12345678901234567890123456789012345678901234567890123456789012345678901234567890')
+        l.insert(0, 'A')
+        lw = ListWindow(l, x=2, y=4, w=20, h=10)
+        lw.Y = 10
+        ListKeyInput()
+
+    Terminal.Name = 'xterm-256color'
+    Curses.run(init=init, wait_time=5)
+#    Curses.run()
+
